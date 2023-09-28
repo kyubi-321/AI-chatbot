808757eb297e040bb47b583fdef4f6d313ca4b1d
unknown <Ankitbadhani102@gmail.com>
2023-09-28 17:44:00 +0530

    Again did some changes

--- app/api/route.ts
+++ app/api/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: Request){
 
     })
 
-    console.log(messages)
+    
 
     const stream = OpenAIStream(response,{
         onCompletion : async (completion : string)=>{
